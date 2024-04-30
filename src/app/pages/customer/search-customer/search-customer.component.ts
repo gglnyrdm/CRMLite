c4d291315ced124610163a82f0f121b6bc255763
Dogukan <dk.dogukankoc@gmail.com>
2024-05-01 01:15:46 +0300

    Add search results component

--- src/app/pages/customer/search-customer/search-customer.component.ts
+++ src/app/pages/customer/search-customer/search-customer.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SearchFilterComponent } from '../../../features/search-filter/search-filter/search-filter.component';
+import { SearchResultsComponent } from '../../../features/search-result/search-results/search-results.component';
 
 @Component({
   selector: 'app-search-customer',
   standalone: true,
   imports: [
     CommonModule,
-    SearchFilterComponent
+    SearchFilterComponent,
+    SearchResultsComponent
   ],
   templateUrl: './search-customer.component.html',
   styleUrl: './search-customer.component.scss',
