45885cdb5bc07db59f8adb175e88a70982812c86
Sevda <58671217+sevdasimsek@users.noreply.github.com>
2024-05-01 00:32:51 +0300

    login form updated

--- src/app/pages/auth/login-page/login-page/login-page.component.ts
+++ src/app/pages/auth/login-page/login-page/login-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SingleFocusLayoutComponent } from '../../../../shared/layouts/single-focus-layout/single-focus-layout/single-focus-layout.component';
+import { LoginFormComponent } from '../../../../features/auth/components/login-form/login-form/login-form.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [SingleFocusLayoutComponent,CommonModule],
+  imports: [SingleFocusLayoutComponent, CommonModule, LoginFormComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
