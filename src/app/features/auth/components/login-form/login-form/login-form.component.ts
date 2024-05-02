db1a3f097f35ad1f5ec44abe00d90ac2316830e1
Sevda <58671217+sevdasimsek@users.noreply.github.com>
2024-05-02 19:52:35 +0300

    css updated, and added show/hide eyes for password

--- src/app/features/auth/components/login-form/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form/login-form.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
+  showPassword: boolean = false;
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
