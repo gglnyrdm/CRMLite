f733ce8432a7da531f7e43098aa993190baad18b
Sevda <58671217+sevdasimsek@users.noreply.github.com>
2024-05-03 11:16:56 +0300

    Activating the button based on user data entry

--- src/app/features/auth/components/login-form/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import test from 'node:test';
 
 @Component({
   selector: 'app-login-form',
@@ -11,9 +12,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
+  username: string = '';
+  password: string = '';
   showPassword: boolean = false;
+  enableSubmit: boolean = false;
+  submitButtonClass: string = '';
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
+  }
+  
+  checkInputs(): void {
+    if (this.username.trim() !== '' && this.password.trim() !== '') {
+      this.enableSubmit = true;
+      this.submitButtonClass = 'active'; 
+    } else {
+      this.enableSubmit = false;
+      this.submitButtonClass = ''; 
+    }
   }
 }
