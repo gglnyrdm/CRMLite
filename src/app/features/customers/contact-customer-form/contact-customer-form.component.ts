371a0c957ac9419289b0413c0f51bc8771355574
gglnyrdm <gulin.yardimoglu@etiya.com>
2024-05-03 09:26:16 +0300

    update contact customer ts

--- src/app/features/customers/contact-customer-form/contact-customer-form.component.ts
+++ src/app/features/customers/contact-customer-form/contact-customer-form.component.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { OnlyNumberInputDirective } from '../../../core/directives/only-number-input.directive';
+import { OnlyLetterDirective } from '../../../core/directives/only-letter.directive';
 
 @Component({
   selector: 'app-contact-customer-form',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule,
+    OnlyNumberInputDirective,
+    OnlyLetterDirective],
   templateUrl: './contact-customer-form.component.html',
   styleUrl: './contact-customer-form.component.scss'
 })
