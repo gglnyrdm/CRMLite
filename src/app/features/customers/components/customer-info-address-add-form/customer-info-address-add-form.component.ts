5795ec8d4926d5fa43ca5395c3ed6f0437988d55
gglnyrdm <gulin.yardimoglu@etiya.com>
2024-05-08 17:47:08 +0300

    add custumer info adress add component

---
+++ src/app/features/customers/components/customer-info-address-add-form/customer-info-address-add-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-info-address-add-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-info-address-add-form.component.html',
+  styleUrl: './customer-info-address-add-form.component.scss'
+})
+export class CustomerInfoAddressAddFormComponent {
+
+}
