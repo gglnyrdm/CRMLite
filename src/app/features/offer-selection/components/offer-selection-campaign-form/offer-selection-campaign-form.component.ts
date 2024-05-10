332fd4ada7d2682ea2a03ad9c6d42c5eecf1a5c5
Sevda <58671217+sevdasimsek@users.noreply.github.com>
2024-05-10 15:23:00 +0300

    created components of offer-selection

---
+++ src/app/features/offer-selection/components/offer-selection-campaign-form/offer-selection-campaign-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-selection-campaign-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './offer-selection-campaign-form.component.html',
+  styleUrl: './offer-selection-campaign-form.component.scss'
+})
+export class OfferSelectionCampaignFormComponent {
+
+}
---
+++ src/app/features/offer-selection/components/offer-selection-catalog-form/offer-selection-catalog-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-selection-catalog-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './offer-selection-catalog-form.component.html',
+  styleUrl: './offer-selection-catalog-form.component.scss'
+})
+export class OfferSelectionCatalogFormComponent {
+
+}
