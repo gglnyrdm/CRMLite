e5af60e163fb47503336e37bab7be502604b8179
Sevda <58671217+sevdasimsek@users.noreply.github.com>
2024-05-13 12:19:43 +0300

    updated offer-selection

---
+++ src/app/features/offer-selection/components/offer-selection-basket/offer-selection-basket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-selection-basket',
+  standalone: true,
+  imports: [],
+  templateUrl: './offer-selection-basket.component.html',
+  styleUrl: './offer-selection-basket.component.scss'
+})
+export class OfferSelectionBasketComponent {
+
+}
