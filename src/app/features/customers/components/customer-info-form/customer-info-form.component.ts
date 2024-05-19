6f8504650c5301d7224cf5855d90e371a6d7d403
Dogukan <dk.dogukankoc@gmail.com>
2024-05-19 19:02:04 +0300

    Add mernis and directives for customer info form component

--- src/app/features/customers/components/customer-info-form/customer-info-form.component.ts
+++ src/app/features/customers/components/customer-info-form/customer-info-form.component.ts
@@ -7,6 +7,9 @@ import { GetCustomerResponse } from '../../models/responses/customer/get-custome
 import { PutCustomerRequest } from '../../models/requests/customer/put-customer-request';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DialogPopupComponent } from '../../../../shared/components/dialog-popup/dialog-popup.component';
+import { CheckNationalityIdentityOnMernis } from '../../models/requests/customer/check-nationality-identity-on-mernis-request';
+import { OnlyLetterDirective } from '../../../../core/directives/only-letter.directive';
+import { OnlyNumberInputDirective } from '../../../../core/directives/only-number-input.directive';
 
 @Component({
   selector: 'app-customer-info-form',
@@ -14,7 +17,9 @@ import { DialogPopupComponent } from '../../../../shared/components/dialog-popup
   imports: [
     CommonModule,
     RouterModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OnlyLetterDirective,
+    OnlyNumberInputDirective
   ],
   templateUrl: './customer-info-form.component.html',
   styleUrls: ['./customer-info-form.component.scss'],
@@ -22,9 +27,14 @@ import { DialogPopupComponent } from '../../../../shared/components/dialog-popup
 })
 export class CustomerInfoFormComponent implements OnInit {
 
+  currentDate = new Date();
+  eighteenYearsAgo = new Date(this.currentDate.getFullYear() - 18, this.currentDate.getMonth(), this.currentDate.getDate());
+  formattedDate = `${this.eighteenYearsAgo.getFullYear()}-${String(this.eighteenYearsAgo.getMonth() + 1).padStart(2, '0')}-${String(this.eighteenYearsAgo.getDate()).padStart(2, '0')}`;
+
   customerId!: string;
   customerInfo!: GetCustomerResponse;
   form!: FormGroup;
+  checkNationalityIdentityOnMernisObject: CheckNationalityIdentityOnMernis;
 
   constructor(
     private fb: FormBuilder,
@@ -44,7 +54,7 @@ export class CustomerInfoFormComponent implements OnInit {
       middleName: ['', [Validators.minLength(2)]],
       birthDate: ['', [Validators.required]],
       fatherName: ['', [Validators.minLength(2)]],
-      nationalityIdentity: ['', [Validators.required]]
+      nationalityIdentity: ['', [Validators.required,Validators.minLength(11)]]
     });
 
     this.activatedRoute.parent?.params.subscribe(params => {
@@ -53,6 +63,58 @@ export class CustomerInfoFormComponent implements OnInit {
       this.getCustomerInfo();
     });
   }
+
+  checkNationalityIdentityOnMernis() {
+    const birthDateValue = this.form.value.birthDate;
+    let birthDate: Date;
+  
+    if (typeof birthDateValue === 'string') {
+      birthDate = new Date(birthDateValue);
+    } else {
+      birthDate = birthDateValue;
+    }
+    this.checkNationalityIdentityOnMernisObject = {
+      nationalityIdentity: this.form.value.nationalityIdentity,
+      firstName: this.checkMiddleName(),
+      lastName: this.form.value.lastName,
+      birthDate: birthDate.getFullYear()
+    };
+  
+    // MERNİS kontrolünü gerçekleştir
+    this.customerApiService.checkNationalityIdentityOnMernis(this.checkNationalityIdentityOnMernisObject).subscribe({
+      next: async (response) => {
+        if (!response) {
+          // MERNİS doğrulaması başarılı, müşteriyi güncelle
+          this.customerApiService.putCustomer(this.customerId, this.form.value).subscribe({
+            next: () => {},
+            error: (error) => {
+              console.error('Error updating customer:', error);
+            },
+            complete: () => {
+              this.openPopup("Changes have been saved.");
+              this.router.navigate(['customer/', this.customerId, 'info']);
+            }
+          });
+        } else {
+          // MERNİS doğrulaması başarısız
+          this.openPopup("You entered an invalid ID.");
+          await this.delay(2500);
+        }
+      },
+      error: (err) => {
+        console.error('Error checking nationality identity on MERNİS:', err);
+      }
+    });
+  }
+
+  checkMiddleName() {
+  if(this.form.value.middleName){
+    return this.form.value.firstName+ ' ' +this.form.value.middleName;
+  }
+  else {
+    return this.form.value.firstName;
+  }
+}
 
   getCustomerInfo(): void {
     this.customerApiService.getById(this.customerId).subscribe({
@@ -87,16 +149,21 @@ export class CustomerInfoFormComponent implements OnInit {
 
   updateCustomer(): void {
     const request: PutCustomerRequest = this.form.value;
-    this.customerApiService.putCustomer(this.customerId, request).subscribe({
-      next: () => {},
-      error: (error) => {
-        console.error('Error', error);
-      },
-      complete: () => {
-        this.openPopup("Changes have been saved.");
-        this.router.navigate(['customer/', this.customerId, 'info']);
-      }
-    });
+  
+    // Eski nationalityIdentity değerini al
+    const previousNationalityIdentity = this.customerInfo.nationalityIdentity;
+  
+    // Yeni nationalityIdentity değerini al
+    const newNationalityIdentity = this.form.get('nationalityIdentity')?.value;
+  
+    // Eğer nationalityIdentity değeri değiştiyse, kontrol et
+    if (previousNationalityIdentity !== newNationalityIdentity) {
+      // CheckNationalityIdentityOnMernis objesini oluştur
+      this.checkNationalityIdentityOnMernis();
+    } else {
+      // Eğer nationalityIdentity değeri değişmediyse, doğrudan MERNİS kontrolünü yap
+      this.checkNationalityIdentityOnMernis();
+    }
   }
 
   onSubmitForm(): void {
@@ -117,9 +184,14 @@ export class CustomerInfoFormComponent implements OnInit {
       data: { message: message },
       panelClass: 'custom-dialog-container'
     });
-
+  
     setTimeout(() => {
       dialogRef.close();
-    }, 2500);
+    }, 2500); 
   }
+  
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
 }
